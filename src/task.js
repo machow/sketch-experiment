d1b20abc12d537eed9da6116d80e2521046f3e5a
Michael Chow <machow@princeton.edu>
2016-05-17 12:21:11 -0400

    clean up task script, tests passing

--- src/task.js
+++ src/task.js
@@ -12,47 +12,59 @@ module.exports = {
                     
 
 function Task(userCanvasId, goalCanvasId, cueText, trialPars){
-    // Set up both canvases
-    var scope = new paper.PaperScope();
-    scope.setup(userCanvasId);
-    scope.view.onFrame = function(){
-        scope.view.draw();
-    };
-    scope.currentStyle = { strokeColor: 'black'};
-
-    var goalScope = new paper.PaperScope(); // will be manually drawn
-    goalScope.setup(goalCanvasId);
-    goalScope.currentStyle = { strokeColor: 'black'};
-
-    scope.activate();
-    
-    console.log(scope);
-    console.log(goalScope);
-
-    // Set up scoring using procrustes transformation
-    var rater = new procrustes()
-
-    // Set up trials themselves
-    var self = {scope, goalScope}
-    self.log = []
-    self.actions = Actions(self.log, [Lines(scope, goalScope, rater.procrustes)]);
-    var drawing = DrawingTrial(scope, self.actions);
-    var intro = IntroTrial(scope, self.actions, cueText);
-    var scoring = ScoringTrial(scope, self.actions);
-    console.log(drawing);
-
-    // each round is a generator that yields promises for each stage
-    self.trials = trialPars.map(el => sequence([intro, drawing(el), scoring]));
-
-    self.run = (cb) => co(function* () {
-        for (let gen of self.trials) yield* gen();
-        yield cb;
-    });
-
+    var self = {
+        // properties
+        scope: createScope(userCanvasId),
+        goalScope: createScope(goalCanvasId),
+        rater: new procrustes(),
+        log: [],
+        actions: null,
+        // actions
+        createActions: (log, scope, goalScope, rater) => 
+            Actions(log, [Lines(scope, goalScope, rater.procrustes)]),
+        replay: (cb) => ReplayTrial(self.scope, self.actions, self.log, cb).start(),
+        // scope and trial functions
+        activateUserScope,
+        createScope,
+        initializeTrials,
+        run
+    }
+    self.actions = self.createActions(self.log, self.scope, self.goalScope, self.rater);
+    self.trials = self.initializeTrials(self.scope, self.actions, cueText);
     self.test = co.wrap(self.trials[0]);
-    self.replay = (cb) => ReplayTrial(scope, self.actions, self.log, cb).start();
-
+    self.activateUserScope();
     return self
+
+    function run(cb){
+        return co(function* () {
+            for (let gen of self.trials) yield* gen();
+            yield cb;
+        });
+    }
+
+    function initializeTrials(scope, actions, cueText){
+        var drawing = DrawingTrial(scope, actions);
+        var intro = IntroTrial(scope, actions, cueText);
+        var scoring = ScoringTrial(scope, actions);
+        return trialPars.map(el => sequence([intro, drawing(el), scoring]));
+    }
+
+    function activateUserScope(){
+        var scope = self.scope
+        scope.view.onFrame = function(){
+            scope.view.draw();
+        };
+        scope.currentStyle = { strokeColor: 'black'};
+        return scope;
+    }
+
+    function createScope(canvasId){
+        var scope = new paper.PaperScope(); // will be manually drawn
+        scope.setup(canvasId);
+        scope.currentStyle = { strokeColor: 'black'};
+        console.log(scope)
+        return scope;
+    }
 }
 
 function sequence(arr){
